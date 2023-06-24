9ec62a297227161abe4e974ce5571db64c85747f
your-denys <denzfg@gmail.com>
2023-06-24 19:53:03 +0300

    loading video

--- src/components/HomeContent/HomeContent.js
+++ src/components/HomeContent/HomeContent.js
@@ -68,7 +68,7 @@ const HomeContent = () => {
           src={video}
           type="video/mp4"
           playsInline
-          onLoadedData={handleVideoLoad} controls
+          onLoadedData={handleVideoLoad}
          style={{ display:videoLoaded ? 'block' : 'none' }}
         />
 
