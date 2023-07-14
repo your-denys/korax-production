1194473d8a38784f69d14cc4df5869a97b9a8137
your-denys <denzfg@gmail.com>
2023-07-15 00:26:36 +0300

    fix + loading

--- src/components/KoraxAIContent/KoraxAIContent.js
+++ src/components/KoraxAIContent/KoraxAIContent.js
@@ -1,16 +1,27 @@
 import './KoraxAIContent.css';
 import { motion } from 'framer-motion';
 import solid from '../../assets/wawes.mp4';
+import { useState } from 'react';
+import poster from '../../assets/wawes-poster.jpg'
+
 
 const KoraxAIContent = () => {
+
+  const [videoLoaded1, setVideoLoaded1] = useState(false);
+
 
   const handleContextMenu = (event) => {
     event.preventDefault();
   };
 
+  const handleVideoLoad1 = () => {
+    setVideoLoaded1(true);
+  };
+
   return (
     <section className="koraxAI">
       <article className='koraxAI-content solid-bg'>
+      {!videoLoaded1 && <img src={poster} className='video' alt="Robot" />}
       <video
         preload='auto'
           autoPlay
@@ -20,6 +31,8 @@ const KoraxAIContent = () => {
           type="video/mp4"
           playsInline
           onContextMenu={handleContextMenu}
+          onLoadedData={handleVideoLoad1}
+          style={{ display: videoLoaded1 ? 'block' : 'none' }}
         />
       </article>
       <article className="koraxAI-content" style={{position: 'relative', zIndex:2222, marginTop:'11rem'}}>
