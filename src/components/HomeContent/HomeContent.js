8aaa7f45ba2fd1fd7a840afde0c78073c40d7d99
your-denys <denzfg@gmail.com>
2023-07-16 15:21:49 +0300

    fix + loading

--- src/components/HomeContent/HomeContent.js
+++ src/components/HomeContent/HomeContent.js
@@ -2,7 +2,7 @@ import Nav from 'react-bootstrap/Nav';
 import './HomeContent.css';
 import video from '../../assets/video.webm';
 // import videoMob from '../../assets/phone-video.mp4';
-import videoMob from '../../assets/../../assets/robot-screen.mp4';
+import videoMob from '../../assets/robot-screen.mp4';
 import { motion } from 'framer-motion';
 import { useSwiper } from 'swiper/react';
 import { useState } from 'react';
