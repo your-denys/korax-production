74ca5bce7de72d4500ed1058badfe94b8ce52507
your-denys <denzfg@gmail.com>
2023-10-03 04:23:12 +0300

    calculator

--- src/components/PremiumHomeContent/PremiumHomeContent.js
+++ src/components/PremiumHomeContent/PremiumHomeContent.js
@@ -11,16 +11,13 @@ const PremiumHomeContent = () => {
   return (
     <section className="premium-content">
       <img src={trading} alt="Trading for everyone" />
-      <p className="premium-description mob">
+      <p className="premium-description ">
         Korax Presents New User-Friendly AI Tool For Succesful Trading
       </p>
       <div className="premium-wrapper">
         <a href='#premium' className="get-access">
          Get Access
         </a>
-      <p className="premium-description pc">
-        Korax Presents New User-Friendly AI Tool For Succesful Trading
-      </p>
         {isMobile ? (
           <Video
             className="premium-video"
